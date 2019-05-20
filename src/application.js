7dc8d1c8a37405615c1d25ffb1812f4e0a3ae64c
pranavs99 <pranavs99@github.com>
2019-05-20 16:38:36 -0700

    trying to get React Bootstrap to work

--- src/application.js
+++ src/application.js
@@ -1,7 +1,7 @@
-import React from "react"
-import Button from "react-bootstrap/Button"
+import React, { Component } from "react"
+import { Button } from "react-bootstrap"
 
-class ThanosButton extends React.Component {
+class ThanosButton extends Component {
     // constructor for thanosApp
     constructor(props) {
         // signifying that the class thanosApp inherits its props
